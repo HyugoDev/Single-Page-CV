befa17c6ec9577d5fffd26777b6c7916871585a7
Hugo Mejias <h.mejias.ugalde53@gmail.com>
2025-04-25 11:02:16 -0600

    refactor: replace <span> with <p> for semantic HTML improvements

--- src/App.jsx
+++ src/App.jsx
@@ -11,38 +11,38 @@ function App() {
         <section className="hoja__section">
           <h1>Hugo Gerardo Mejias Ugalde</h1>
           <h2>Junior Full Stack Developer</h2>
-          <span className="text">
+          <p className="text">
             Miramar, Puntarenas, Costa Rica<br />
             (506) 8710-0664 <br />
             hyugodev@gmail.com<br />
-          </span>
+          </p>
         </section >
         {/* Skills */}
         <section className="hoja__section">
           <h2>Skills</h2>
-          <span className="text">
+          <p className="text">
             HTML, CSS, JavaScript, React, Git, Express, Java, MySQL, PostgreSQL, Tailwind CSS
-          </span>
+          </p>
         </section >
         {/* Education */}
         <section className="hoja__section">
           <h2>Education</h2>
           <h3>Universidad Estatal a Distancia, Puntarenas - Diplomado</h3>
-          <span className="text">
+          <p className="text">
             Enero 2020 to Septiembre 2023<br />
             Diplomado en Ingenieria Informatica
-          </span>
+          </p>
           <h3>Universidad Estatal a Distancia, Puntarenas - Bachillerato</h3>
-          <span className="text">
+          <p className="text">
             Enero 2023 to Agosto 2025<br />
             Bachillerato en Ingenieria Informatica
-          </span>
+          </p>
         </section >
         {/* Experience */}
         <section className="hoja__section">
           <h2>Experience</h2>
           <h3>Municipalidad Montes de Oro, Miramar - Developer</h3>
-          <span className="text">
+          <p className="text">
             Enero 2024 to Noviembre 2024<br />
             <ul>
               <li>Desarrollo de una aplicación web para la gestión de Retenciones a Proveedores.</li>
@@ -50,15 +50,15 @@ function App() {
               <li>Colaboración en un equipo de Contabilidad para el desarrollo de software.</li>
             </ul>
             Skills: JavaScript, React, Nextjs, Express, MySQL, Tailwind CSS
-          </span>
+          </p>
         </section >
         {/* Link */}
         <section className="hoja__section">
           <h2>Across the Internet</h2>
-          <span className="text">
+          <p className="text">
             <a href="https://www.linkedin.com/in/huguitodev/" target="_blank">LinkedIn</a><br />
             <a href="https://github.com/HyugoDev" target="_blank">GitHub</a><br />
-          </span>
+          </p>
         </section >
       </div>
     </div>
